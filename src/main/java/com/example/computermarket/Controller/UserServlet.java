a780ebcd888d10c3f059e706c27351f511b26f00
hoanghaibk93 <hoanghaibk93@gmail.com>
2023-03-17 17:13:48 +0700

    Hai update method edit, display user

--- src/main/java/com/example/computermarket/Controller/UserServlet.java
+++ src/main/java/com/example/computermarket/Controller/UserServlet.java
@@ -1,19 +1,106 @@
 package com.example.computermarket.Controller;
 
+import com.example.computermarket.Model.User;
+import com.example.computermarket.Service.IUserService;
+import com.example.computermarket.Service.UserService;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
+import java.awt.image.PackedColorModel;
 import java.io.IOException;
+import java.util.List;
 
-@WebServlet(name = "UserServlet", value = "/UserServlet")
+@WebServlet(name = "UserServlet", value = "/user")
 public class UserServlet extends HttpServlet {
+    IUserService service = new UserService();
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "";
+        }
+        switch (action) {
+            case "create":
+                break;
+            case "update":
+                showUpdateUser(request, response);
+                break;
+            case "delete":
+                break;
+            case "view":
+                break;
+            default:
+                showListUser(request, response);
+                break;
+        }
+    }
 
+    private void showUpdateUser(HttpServletRequest request, HttpServletResponse response) {
+        int id = Integer.parseInt(request.getParameter("id"));
+        User user = service.findUserById(id);
+//        if (user == null) {
+//            try {
+//                response.sendRedirect("/user/error-404.jsp");
+//            } catch (IOException e) {
+//                throw new RuntimeException(e);
+//            }
+//        } else {
+            request.setAttribute("user", user);
+            try {
+                request.getRequestDispatcher("/user/edit_user.jsp").forward(request, response);
+            } catch (ServletException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+//        }
+    }
+
+    private void showListUser(HttpServletRequest request, HttpServletResponse response) {
+        List<User> userList = service.findAllUser();
+        request.setAttribute("userList", userList);
+        try {
+            request.getRequestDispatcher("/user/list_user.jsp").forward(request, response);
+        } catch (ServletException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "";
+        }
+        switch (action) {
+            case "create":
+                break;
+            case "update":
+                updateUser(request, response);
+                break;
+            case "delete":
+                break;
+            case "view":
+                break;
+            default:
+                break;
+        }
+    }
 
+    private void updateUser(HttpServletRequest request, HttpServletResponse response) {
+     int id = Integer.parseInt(request.getParameter("id"));
+     User user = service.findUserById(id);
+     service.update(user);
+        try {
+            request.getRequestDispatcher("/user/edit.jsp").forward(request,response);
+        } catch (ServletException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/webapp/user/list_user.jsp
+++ src/main/webapp/user/list_user.jsp
@@ -6,6 +6,7 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
 <head>
     <title>List User</title>
@@ -13,6 +14,29 @@
     <script src="../bootstrap-5.1.3-dist/js/bootstrap.min.js"></script>
 </head>
 <body>
-
+<table class="table table-hover table-inverse table-responsive">
+    <thead class="thead-inverse">
+    <tr>
+        <th>ID</th>
+        <th>NAME</th>
+        <th>EMAIL</th>
+        <th>PASSWORD</th>
+        <th>EDIT</th>
+        <td>DELETE</td>
+    </tr>
+    </thead>
+    <tbody>
+    <c:forEach items="${userList}" var="user">
+    <tr>
+        <td scope="row">${user.id}</td>
+        <td><a href="/user?action=view&id=${user.id}">${user.name}</a></td>
+        <td>${user.email}</td>
+        <td>${user.password}</td>
+        <td><a href="/user?action=update&id=${user.id}">Edit</a></td>
+        <td><a href="/user?action=delete&id=${user.id}">Delete</a></td>
+    </tr>
+    </c:forEach>
+    </tbody>
+</table>
 </body>
 </html>
